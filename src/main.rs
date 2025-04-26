b3c825140e73ccc38363fa4f85760ec80607f757
haike0513 <haike0513@gmail.com>
2025-04-26 08:43:55 +0800

    feat: add main train

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
-use  burn::backend::{self, rocm};
+use  burn::backend::{self, rocm, Autodiff};
 
 use burn::data::dataset::Dataset;
+use burn::optim::AdamConfig;
+use model::ModelConfig;
+use training::TrainingConfig;
 pub mod model;
 pub mod inference;
 pub mod data;
 pub mod training;
-#[tokio::main]
-pub async fn main() {
+// #[tokio::main]
+pub  fn main() {
     type MyBackend = backend::Rocm<f32, i32>;
     let device = rocm::HipDevice::new(0);
     println!("Hello, world! {:#?}", device);
+
+    type MyAutodiffBackend = Autodiff<MyBackend>;
+
+    // All the training artifacts will be saved in this directory
     let artifact_dir = "/tmp/guide";
+
+    // Train the model
+    training::train::<MyAutodiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device.clone(),
+    );
 
         // Infer the model
         inference::infer::<MyBackend>(
