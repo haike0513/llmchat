115d5be3c73256e9dce12754f5db2657fe9305de
haike0513 <haike0513@gmail.com>
2025-04-29 07:27:54 +0800

    add train use cpu

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,19 @@ type Elem = f32;
 type MyBackend = backend::Rocm<f32, i32>;
 type MyAutodiffBackend = Autodiff<MyBackend>;
 
-// type Backend = burn::backend::Autodiff<burn::backend::LibTorch<Elem>>;
+type Backend = burn::backend::Autodiff<burn::backend::LibTorch<Elem>>;
 
 fn main() {
 
     let device = rocm::HipDevice::new(0);
+    let device = burn::tensor::Device::<Backend>::Cpu;
     let config = ExperimentConfig::new(
         burn::nn::transformer::TransformerEncoderConfig::new(384, 1536, 12, 6)
             .with_norm_first(true),
         burn::optim::AdamConfig::new().with_weight_decay(Some(WeightDecayConfig::new(1.0e-6))),
     );
 
-    llmchat::training::train::<MyAutodiffBackend, DbPediaDataset>(
+    llmchat::training::train::<Backend, DbPediaDataset>(
         device,
         DbPediaDataset::train(),
         DbPediaDataset::test(),
