e96ab8ed64ba88fbb111dce9b428c7b6e710ad1c
haike0513 <haike0513@gmail.com>
2025-04-29 10:00:23 +0800

    feat: add train

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,21 @@ type Elem = f32;
 type MyBackend = backend::Rocm<f32, i32>;
 type MyAutodiffBackend = Autodiff<MyBackend>;
 
-type Backend = burn::backend::Autodiff<burn::backend::LibTorch<Elem>>;
+type TorchBackend = burn::backend::Autodiff<burn::backend::LibTorch<Elem>>;
+type Backend = MyAutodiffBackend;
+
 
 fn main() {
 
     let device = rocm::HipDevice::new(0);
-    let device = burn::tensor::Device::<Backend>::Cpu;
+    let device = burn::tensor::Device::<TorchBackend>::Cpu;
     let config = ExperimentConfig::new(
         burn::nn::transformer::TransformerEncoderConfig::new(384, 1536, 12, 6)
             .with_norm_first(true),
         burn::optim::AdamConfig::new().with_weight_decay(Some(WeightDecayConfig::new(1.0e-6))),
     );
 
-    llmchat::training::train::<Backend, DbPediaDataset>(
+    llmchat::training::train::<TorchBackend, DbPediaDataset>(
         device,
         DbPediaDataset::train(),
         DbPediaDataset::test(),
