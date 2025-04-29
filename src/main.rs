57dbe2e2c609d7277b04214bf8370cec2d0ba59a
haike0513 <haike0513@gmail.com>
2025-04-29 20:54:52 +0800

    feat: add wgpu train support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use burn::{backend::{self, rocm, Autodiff}, optim::decay::WeightDecayConfig};
+use burn::{backend::{self, rocm, wgpu::WgpuRuntime, Autodiff}, optim::decay::WeightDecayConfig};
 use llmchat::{DbPediaDataset, training::ExperimentConfig};
 
 #[cfg(feature = "f16")]
@@ -6,24 +6,32 @@ type Elem = burn::tensor::f16;
 #[cfg(not(feature = "f16"))]
 type Elem = f32;
 
-type MyBackend = backend::Rocm<f32, i32>;
+// type MyBackend = backend::Rocm<f32, i32>;
+type MyBackend = burn::backend::wgpu::CubeBackend<WgpuRuntime, f32, i32, u32>;
 type MyAutodiffBackend = Autodiff<MyBackend>;
 
 type TorchBackend = burn::backend::Autodiff<burn::backend::LibTorch<Elem>>;
 type Backend = MyAutodiffBackend;
+type WGPUBackend = MyAutodiffBackend;
+
+
+
 
 
 fn main() {
 
     let device = rocm::HipDevice::new(0);
     let device = burn::tensor::Device::<TorchBackend>::Cpu;
+    let device = burn::tensor::Device::<TorchBackend>::Cpu;
+    let device = Default::default();
+
     let config = ExperimentConfig::new(
         burn::nn::transformer::TransformerEncoderConfig::new(384, 1536, 12, 6)
             .with_norm_first(true),
         burn::optim::AdamConfig::new().with_weight_decay(Some(WeightDecayConfig::new(1.0e-6))),
     );
 
-    llmchat::training::train::<TorchBackend, DbPediaDataset>(
+    llmchat::training::train::<WGPUBackend, DbPediaDataset>(
         device,
         DbPediaDataset::train(),
         DbPediaDataset::test(),
