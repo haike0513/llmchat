cfe57910e3312bcaeb42b99611538b7064a48cc6
haike0513 <haike0513@gmail.com>
2025-05-03 14:34:16 +0800

    feat: add single load model

--- src/ext.rs
+++ src/ext.rs
@@ -1,12 +1,15 @@
 use std::sync::Arc;
 
-use crate::extensions::llm::LLMGenerate;
 use crate::tch_cpu;
+use crate::{extensions::llm::LLMGenerate, tokenizer::SentiencePieceTokenizer};
+use burn::backend::LibTorch;
 use clap::Parser;
+use tokio::sync::Mutex;
 use tokio::{pin, sync::mpsc};
 
 use futures::{
-    stream::{unfold, BoxStream, Stream}, StreamExt
+    StreamExt,
+    stream::{BoxStream, Stream, unfold},
 };
 use tokio_stream::wrappers::ReceiverStream;
 
@@ -14,59 +17,96 @@ use crate::chat;
 
 pub mod llama;
 
-#[derive(Clone, Debug)]
-pub struct LlamaModel {}
+use crate::llama::{Llama, LlamaConfig};
+
+#[derive()]
+pub struct LlamaModel {
+    pub llama: Option<Llama<LibTorch, SentiencePieceTokenizer>>,
+}
+
+unsafe impl Sync for LlamaModel {}
+
+unsafe impl Send for LlamaModel {}
+use crate::sampling::{Sampler, TopP};
 
 impl LlamaModel {
-    pub async fn generate(&self, prompt: String, tx: mpsc::Sender<String>) {
-        llama::run(tx, prompt).await;
+    pub async fn generate(&mut self, prompt: String, tx: mpsc::Sender<String>) {
+        // llama::run(tx, prompt).await;
+        let mut sampler = Sampler::TopP(TopP::new(0.9, 42));
+
+        let mut llama = self.llama.as_mut().unwrap();
+        crate::chat::generate(
+            llama,
+            prompt.as_str(),
+            65,
+            0.6,
+            &mut sampler,
+            tx,
+        )
+        .await;
+    }
+    pub async fn load_model(&mut self) {
+        let device = burn::tensor::Device::<LibTorch>::Cpu;
+        let model = LlamaConfig::tiny_llama_pretrained::<LibTorch>(1024, &device).unwrap();
+        self.llama = Some(model);
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 
 pub struct LlamaExtension {
-    model: Arc<LlamaModel>,
+    model: Arc<Mutex<LlamaModel>>,
 }
 
 impl LlamaExtension {
     pub fn new() -> Self {
-        let model = LlamaModel {};
+        let model = LlamaModel { llama: None };
         Self {
-            model: Arc::new(model),
+            model: Arc::new(Mutex::new(model)),
         }
     }
-    pub fn generate_llm(&self, prompt: String) -> impl Stream<Item = String> + Send + 'static {
-        let (tx, mut rx) = tokio::sync::mpsc::channel::<String>(100);
-
-        let instance = self.model.clone();
-
-        tokio::spawn(async move {
-            instance.generate(prompt.clone(), tx).await;
-            // instance.generate_with_prompt(prompt, tx).await;
-        });
-
-        // use async_stream::stream;
-   
-        // let s = stream! {
-        //     while let Some(value) =  rx.recv().await {
-        //         yield value
-        //     }
-        // };
-
-        // // pin!(s);
-        // s
-
-        let rs = ReceiverStream::new(rx);
-        rs
+    pub async fn generate_token(&self, prompt: String, tx: mpsc::Sender<String>) {
+        let mut model = self.model.lock().await;
+        if model.llama.is_none() {
+            model.load_model().await;
+        }
+        model.generate(prompt, tx).await;
+        // model.generate(prompt, tx).await;
+        // let mut instance = self.model.lock().await;
+        // if instance.llama.is_none() {
+        //     instance.load_model().await;
+        // }
+        // instance.generate(prompt, tx).await;
     }
-    pub async fn generate_with_prompt(
-        &self,
-        prompt: String,
-        tx: tokio::sync::mpsc::Sender<String>,
-    ) {
-        self.model.generate(prompt, tx).await;
-    }
+
+    // pub fn generate_llm(&self, prompt: String) -> impl Stream<Item = String> + Send + 'static {
+    //     let (tx, mut rx) = tokio::sync::mpsc::channel::<String>(100);
+
+    //     let instance = self.model.clone();
+
+    //     tokio::spawn(async move {
+    //         // let mut instance = instance.lock().await;
+    //         // if instance.llama.is_none() {
+    //         //     instance.load_model().await;
+    //         // }
+    //         let prompt = prompt.clone();
+    //         instance.lock().await.generate(prompt.clone(), tx).await;
+    //     });
+
+    //     // use async_stream::stream;
+
+    //     // let s = stream! {
+    //     //     while let Some(value) =  rx.recv().await {
+    //     //         yield value
+    //     //     }
+    //     // };
+
+    //     // // pin!(s);
+    //     // s
+
+    //     let rs = ReceiverStream::new(rx);
+    //     rs
+    // }
 }
 
 impl LLMGenerate for LlamaExtension {
@@ -75,16 +115,18 @@ impl LLMGenerate for LlamaExtension {
     }
 
     fn generate(&self, prompt: String) -> futures::stream::BoxStream<'static, String> {
-        self.generate_llm(prompt).boxed()
-        // let (tx, rx) = tokio::sync::mpsc::channel(32);
-
-        // let args = chat::Config::parse();
-        // let g = tch_cpu::run(args, tx);
-        // let stream = tokio_stream::wrappers::ReceiverStream::new(rx);
-        // let ss = stream.map(|s|{
-        //     let a = s.unwrap();
-        //     "".to_string()
-        // }).boxed();
-        // ss
+        let (tx, mut rx) = tokio::sync::mpsc::channel::<String>(100);
+        let instance = self.clone();
+        tokio::spawn(async move {
+            // let mut instance = instance.lock().await;
+            // if instance.llama.is_none() {
+            //     instance.load_model().await;
+            // }
+            let prompt = prompt.clone();
+            instance.generate_token(prompt, tx).await;
+            // instance.lock().await.generate(prompt.clone(), tx).await;
+        });
+        // self.generate_llm(prompt).boxed()
+        ReceiverStream::new(rx).boxed()
     }
 }
